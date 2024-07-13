ec6ec83760a69367ed732b1317cf65b52185dd9e
Isaac <jarias30680@ufide.ac.cr>
2024-07-12 21:28:50 -0600

    avance1

--- views/assets/js/index.js
+++ views/assets/js/index.js
@@ -33,4 +33,45 @@ sr.reveal('.inf', {
 });
 
 
+sr.reveal('.IMG1', {
+    duration: 3500,
+    origin: 'left',
+    distance:'400px',
+    delay:100,
+    reset: true
+});
+
+sr.reveal('.IMG2', {
+    duration: 3500,
+    origin: 'bottom',
+    distance:'100px',
+    delay:1000,
+    reset: true
+});
+
+sr.reveal('.IMG3', {
+    duration: 3500,
+    origin: 'right',
+    distance:'400px',
+    delay:500,
+    reset: true
+});
+sr.reveal('.mis', {
+    duration: 3500,
+    origin: 'left',
+    distance:'400px',
+    delay:200,
+    reset: true
+});
+sr.reveal('.vis', {
+    duration: 3500,
+    origin: 'right',
+    distance:'400px',
+    delay:500,
+    reset: true
+});
+
+
+AOS.init();
+
 
--- views/index.php
+++ views/index.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Inicio</title>
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   <link rel="stylesheet" href="./assets/css/index.css">
 </head>
 
@@ -16,41 +17,60 @@
   <div id="carouselExampleAutoplaying" class="carousel slide small-carousel" data-bs-ride="carousel">
 
     <div class="carousel-inner">
-      <div class="carousel-item active">
+    <div class="carousel-item active">
+        <img src="./assets/img/imagenMenu.jpeg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
         <img src="https://cloudfront-us-east-1.images.arcpublishing.com/gruponacion/ZMOHZAW37ZBNDCOMXBTPGNNCUI.jpeg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="https://global.unitednations.entermediadb.net/assets/mediadb/services/module/asset/downloads/preset/Libraries/Production+Library/07-05-2021-IOM-Costa+Rica-Saprissa+School+of+Values.jpg/image770x420cropped.jpg" class="d-block w-100" alt="...">
+        <img src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/1634A/production/_90545909_gettyimages-493198732.jpg.webp" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
         <img src="https://www.nacion.com/resizer/emXMBDn_WYhaeOaJpt2C1POrPg4=/1440x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/gruponacion/JTMPFYZMWNEMBBO2ON4KNTP2QE.jpg" class="d-block w-100" alt="...">
       </div>
     </div>
 
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
 
+  </div>
+  <div class="inmi">
+    <h1>INMIGRAWEB</h1>
+    <div class="img " data-aos="fade-up"
+    data-aos-anchor-placement="top-bottom">
+    <img src="https://static.euronews.com/articles/stories/07/42/43/48/1440x810_cmsv2_430cf0dd-b341-5836-974d-91778f2ed916-7424348.jpg" class="IMG1" alt="...">
+    <img src="https://revistaelestornudo.com/wp-content/uploads/2022/09/20220719_6_1-1.jpg"  class="IMG2"alt="...">
+    <img src="https://hias.org/wp-content/uploads/GettyImages-1252557662.jpg" class="IMG3" alt="...">
+    </div>
   </div>
 
   <div class="inf">
-    <div>
+    <div class=" col-mb-6">
       <h1 class="titulo">Quienes</h1>
       <h1 class="titulo">Somos?</h1>
     </div>
-    <div class="text">
-      <p>Bienvenidos a INMIGRAWEB, su aliado confiable en el camino hacia nuevas oportunidades y destinos. 
-        Somos una empresa comprometida con brindar apoyo integral a los inmigrantes en Costa Rica que desean 
-        continuar su viaje hacia Panamá o Nicaragua. Nuestro propósito es facilitar su traslado de manera segura y eficiente, 
+    <div class="text col-mb-6">
+      <p">Bienvenidos a INMIGRAWEB, su aliado confiable en el camino hacia nuevas oportunidades y destinos.
+        Somos una empresa comprometida con brindar apoyo integral a los inmigrantes en Costa Rica que desean
+        continuar su viaje hacia Panamá o Nicaragua. Nuestro propósito es facilitar su traslado de manera segura y eficiente,
         asegurando que cada paso de su recorrido esté respaldado por profesionales experimentados y comprometidos con su bienestar.</p>
     </div>
-  </div>
 
+  </div>
+  </div>
+  <!--cosas para agregar al nuevo commit -->
+  <div class="vm col-md-12 mx-auto">
+    <div class="vision ">
+      <h1 class="mis">Misión:</h1> 
+      <p class="mis">InmigraWeb se dedica a facilitar el viaje de los inmigrantes en Costa Rica brindándoles información crucial, asesoría legal, y apoyo logístico. Nos comprometemos a ofrecer un servicio confiable y accesible que empodere a los inmigrantes, ayudándolos a superar los desafíos del camino y a alcanzar sus metas de manera segura y digna.</p>
+      
+      
+
+      <h1 class="vis">Visión:</h1>
+      <p class="vis">Ser la plataforma líder en Costa Rica para apoyar a los inmigrantes en su travesía, proporcionando recursos, orientación y asistencia integral para que puedan continuar su viaje de manera segura y efectiva, con el objetivo de alcanzar la frontera y más allá.</p>
+
+    </div>
+  </div>
+  <p class="frase">"En cada persona que conocemos hay una historia que merece ser escuchada y un futuro que merece ser apoyado."</p>
 
 
 
@@ -69,6 +89,7 @@
     ?>
   </footer>
 </body>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script> <!--/*Para futuro apartado de comentarios*/-->
 <script src="https://unpkg.com/scrollreveal"></script>
 <script src="./assets/js/index.js"></script>
