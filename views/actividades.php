91246c3fa3c7be0222a7901a8d046fadde9bd129
DanoLopez <140762214+DanoLopez@users.noreply.github.com>
2024-07-15 20:00:16 +0000

    actividades

--- views/actividades.php
+++ views/actividades.php
@@ -4,17 +4,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actividades</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="actividades.css"> 
 </head>
 <body>
-<?php
-include 'plantilla.php';
-?>
+    <?php include 'plantilla.php'; ?>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://www.recogemostususados.com/images/servicios/recogida-de-ropa.jpg" class="card-img-top" alt="Imagen actividad">
+                    <div class="card-body">
+                        <h5 class="card-title">Recolección de prendas</h5>
+                        <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
+                        <p class="card-text">Teléfono: +506 8888-8888</p>
+                        <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
+                    </div>
+                    <div class="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15720.035903368916!2d-84.0350103!3d9.9332099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3f47ea4ff37%3A0x7a7818a6a9e5c90c!2sUniversidad%20Fid%C3%A9litas!5e0!3m2!1ses!2scr!4v1721068241124!5m2!1ses!2scr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+            </div>
 
-<footer>
-<?php
-include 'plantillafooter.php';
-?>
-</footer>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1y4LSY25WuwLUrk38XhwbaDDppr8XsKFhFg&s" class="card-img-top" alt="Imagen actividad">
+                    <div class="card-body">
+                        <h5 class="card-title">Recolección de víveres no perecederos</h5>
+                        <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
+                        <p class="card-text">Teléfono: +506 8888-8888</p>
+                        <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
+                    </div>
+                    <div class="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15720.035903368916!2d-84.0350103!3d9.9332099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3f47ea4ff37%3A0x7a7818a6a9e5c90c!2sUniversidad%20Fid%C3%A9litas!5e0!3m2!1ses!2scr!4v1721068241124!5m2!1ses!2scr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR60W_-46cf_mNiRJ25M4p-xuUy8SfkUKMMSA&s" class="card-img-top" alt="Imagen actividad">
+                    <div class="card-body">
+                        <h5 class="card-title">Comedor público</h5>
+                        <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
+                        <p class="card-text">Teléfono: +506 8888-8888</p>
+                        <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
+                    </div>
+                    <div class="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15720.035903368916!2d-84.0350103!3d9.9332099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3f47ea4ff37%3A0x7a7818a6a9e5c90c!2sUniversidad%20Fid%C3%A9litas!5e0!3m2!1ses!2scr!4v1721068241124!5m2!1ses!2scr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <?php include 'plantillafooter.php'; ?>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
