0e9471b44a8537e6caed68866ec86f5075867632
Isaac <jarias30680@ufide.ac.cr>
2024-07-16 15:02:42 -0600

    actividad fin

--- views/actividades.php
+++ views/actividades.php
@@ -20,35 +20,32 @@
 
     <div class="pagina">
 
-        <div class="infoActividades">
+        <div class="infoActividades col-mb-9">
             <h1>Actividades</h1>
-            <p>se han establecido zonas de actividades donde la comunidad puede donar dinero o canastas de víveres esenciales para los inmigrantes.
+            <p style="width: 40%; text-align: center; margin: auto;">se han establecido zonas de actividades donde la comunidad puede donar dinero o canastas de víveres esenciales para los inmigrantes.
                 Estas áreas están diseñadas para facilitar la participación de cualquier persona interesada en ayudar.</p>
 
             <div class="infoextra">
-                <div>
-                    <h1>Recaudación de Fondos y Voluntariado</h1>
-                    <p>La organización realiza diversas iniciativas de recaudación de fondos, incluyendo eventos comunitarios y campañas en línea,
-                         para mantener y ampliar sus actividades. Además, ofrece oportunidades de voluntariado, permitiendo a las personas contribuir
-                          de manera directa en la logística, distribución de suministros y apoyo emocional a los inmigrantes.</p>
 
-                </div>
 
                 <div>
-                    <h1>Apoyo y Guía para Voluntarios</h1>
-                    <p>INMIGRAWEB proporciona capacitación y orientación a los nuevos voluntarios, asegurando que estén bien preparados para su rol y
-                         puedan interactuar de manera efectiva con los inmigrantes.</p>
+                    <h1>Apoyos y Guías para</h1>
+                    <p style="width: 80%; text-align: center; margin: auto;">INMIGRAWEB proporciona capacitación y orientación a los nuevos voluntarios, asegurando que estén bien preparados para su rol y
+                        puedan interactuar de manera efectiva con los inmigrantes.</p>
 
 
                 </div>
-
                 <div>
-                    <h1>Compromiso con la Dignidad y el Respeto</h1>
-                    <p>INMIGRAWEB mantiene un firme compromiso con la dignidad y el respeto hacia los inmigrantes,
-                         trabajando incansablemente para proporcionar el trato humano y el apoyo necesario para
-                          superar los desafíos de su travesía.</p>
-
-
+                    <h1>Recaudaciones</h1>
+                    <p style="width: 50%; text-align: center; margin: auto;">La organización realiza diversas iniciativas de recaudación de fondos, incluyendo eventos comunitarios y campañas en línea,
+                        para mantener y ampliar sus actividades. Además, ofrece oportunidades de voluntariado, permitiendo a las personas contribuir
+                        de manera directa en la logística, distribución de suministros y apoyo emocional a los inmigrantes.</p>
+                </div>
+                <div>
+                    <h1>Dignidad y Respeto</h1>
+                    <p style="width: 80%; text-align: center; margin: auto;">INMIGRAWEB mantiene un firme compromiso con la dignidad y el respeto hacia los inmigrantes,
+                        trabajando incansablemente para proporcionar el trato humano y el apoyo necesario para
+                        superar los desafíos de su travesía.</p>
                 </div>
 
 
@@ -56,15 +53,10 @@
             </div>
 
         </div>
-
-
-
-
-
-
-        <div class="container">
+        <h1 style="text-align: center; margin-bottom: 80px;">Proximas actividades</h1>
+        <div class="container" style="margin-bottom: 5%;">
             <div class="row">
-                <div class="col-md-4 mb-4">
+                <div class="col-md-3 mb-3">
                     <div class="card">
                         <img src="https://www.recogemostususados.com/images/servicios/recogida-de-ropa.jpg" class="card-img-top" alt="Imagen actividad">
                         <div class="card-body">
@@ -80,7 +72,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-4 mb-4">
+                <div class="col-md-3 mb-3">
                     <div class="card">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1y4LSY25WuwLUrk38XhwbaDDppr8XsKFhFg&s" class="card-img-top" alt="Imagen actividad">
                         <div class="card-body">
@@ -96,7 +88,7 @@
                     </div>
                 </div>
 
-                <div class="col-md-4 mb-4">
+                <div class="col-md-3 mb-3">
                     <div class="card">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR60W_-46cf_mNiRJ25M4p-xuUy8SfkUKMMSA&s" class="card-img-top" alt="Imagen actividad">
                         <div class="card-body">
@@ -111,9 +103,33 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-md-3 mb-3">
+                    <div class="card">
+                        <img src="https://png.pngtree.com/background/20220728/original/pngtree-soccer-football-championship-vector-background-with-sports-ball-and-goals-picture-image_1848497.jpg" class="card-img-top" alt="Imagen actividad">
+                        <div class="card-body">
+                            <h5 class="card-title">Recolección de víveres no perecederos</h5>
+                            <p class="card-text">domingo 8 de septiembre</p>
+                            <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
+                            <p class="card-text">Teléfono: +506 8888-8888</p>
+                            <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
+                        </div>
+                        <div class="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d744.6308668749604!2d-84.04141672938339!3d9.91852756245996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3dc248f0349%3A0xb39c8908379bdaf1!2sPlaza%20de%20Jose%20Mar%C3%ADa%20Zeled%C3%B3n%20(AD%20Asturias)!5e0!3m2!1ses!2scr!4v1721163221299!5m2!1ses!2scr" width="100" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+
+
+
+
+
+
+
+
+
+
     <footer>
         <?php include 'plantillafooter.php'; ?>
     </footer>
