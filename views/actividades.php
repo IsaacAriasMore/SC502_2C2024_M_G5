78eb8e1c60bfdb173f5f1f89640aef11d3e4823d
Isaac <jarias30680@ufide.ac.cr>
2024-07-16 00:19:45 -0600

    arreglo actividades sin terminar

--- views/actividades.php
+++ views/actividades.php
@@ -1,67 +1,119 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actividades</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="actividades.css"> 
+    <link rel="stylesheet" href="./assets/css/actividades.css">
 </head>
+
 <body>
     <?php include 'plantilla.php'; ?>
+    <div class="imgin">
+        <img src="./assets/img/imagenMenu.jpeg" class="d-block w-100 h-50 fixed-top z-n1" alt="imagen1">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="https://www.recogemostususados.com/images/servicios/recogida-de-ropa.jpg" class="card-img-top" alt="Imagen actividad">
-                    <div class="card-body">
-                        <h5 class="card-title">Recolección de prendas</h5>
-                        <p class="card-text">Sábado 27 de julio</p>
-                        <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
-                        <p class="card-text">Teléfono: +506 8888-8888</p>
-                        <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
-                    </div>
-                    <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15720.035903368916!2d-84.0350103!3d9.9332099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3f47ea4ff37%3A0x7a7818a6a9e5c90c!2sUniversidad%20Fid%C3%A9litas!5e0!3m2!1ses!2scr!4v1721068241124!5m2!1ses!2scr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                    </div>
+    </div>
+
+
+
+    <div class="pagina">
+
+        <div class="infoActividades">
+            <h1>Actividades</h1>
+            <p>se han establecido zonas de actividades donde la comunidad puede donar dinero o canastas de víveres esenciales para los inmigrantes.
+                Estas áreas están diseñadas para facilitar la participación de cualquier persona interesada en ayudar.</p>
+
+            <div class="infoextra">
+                <div>
+                    <h1>Recaudación de Fondos y Voluntariado</h1>
+                    <p>La organización realiza diversas iniciativas de recaudación de fondos, incluyendo eventos comunitarios y campañas en línea,
+                         para mantener y ampliar sus actividades. Además, ofrece oportunidades de voluntariado, permitiendo a las personas contribuir
+                          de manera directa en la logística, distribución de suministros y apoyo emocional a los inmigrantes.</p>
+
                 </div>
+
+                <div>
+                    <h1>Apoyo y Guía para Voluntarios</h1>
+                    <p>INMIGRAWEB proporciona capacitación y orientación a los nuevos voluntarios, asegurando que estén bien preparados para su rol y
+                         puedan interactuar de manera efectiva con los inmigrantes.</p>
+
+
+                </div>
+
+                <div>
+                    <h1>Compromiso con la Dignidad y el Respeto</h1>
+                    <p>INMIGRAWEB mantiene un firme compromiso con la dignidad y el respeto hacia los inmigrantes,
+                         trabajando incansablemente para proporcionar el trato humano y el apoyo necesario para
+                          superar los desafíos de su travesía.</p>
+
+
+                </div>
+
+
+
             </div>
 
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1y4LSY25WuwLUrk38XhwbaDDppr8XsKFhFg&s" class="card-img-top" alt="Imagen actividad">
-                    <div class="card-body">
-                        <h5 class="card-title">Recolección de víveres no perecederos</h5>
-                        <p class="card-text">Sábado 10 de agosto</p>
-                        <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
-                        <p class="card-text">Teléfono: +506 8888-8888</p>
-                        <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
-                    </div>
-                    <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15720.035903368916!2d-84.0350103!3d9.9332099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3f47ea4ff37%3A0x7a7818a6a9e5c90c!2sUniversidad%20Fid%C3%A9litas!5e0!3m2!1ses!2scr!4v1721068241124!5m2!1ses!2scr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+
+
+
+
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://www.recogemostususados.com/images/servicios/recogida-de-ropa.jpg" class="card-img-top" alt="Imagen actividad">
+                        <div class="card-body">
+                            <h5 class="card-title">Recolección de prendas</h5>
+                            <p class="card-text">Sábado 27 de julio</p>
+                            <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
+                            <p class="card-text">Teléfono: +506 8888-8888</p>
+                            <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
+                        </div>
+                        <div class="map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15720.035903368916!2d-84.0350103!3d9.9332099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3f47ea4ff37%3A0x7a7818a6a9e5c90c!2sUniversidad%20Fid%C3%A9litas!5e0!3m2!1ses!2scr!4v1721068241124!5m2!1ses!2scr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR60W_-46cf_mNiRJ25M4p-xuUy8SfkUKMMSA&s" class="card-img-top" alt="Imagen actividad">
-                    <div class="card-body">
-                        <h5 class="card-title">Comedor público</h5>
-                        <p class="card-text">Sábado 31 de agosto</p>
-                        <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
-                        <p class="card-text">Teléfono: +506 8888-8888</p>
-                        <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1y4LSY25WuwLUrk38XhwbaDDppr8XsKFhFg&s" class="card-img-top" alt="Imagen actividad">
+                        <div class="card-body">
+                            <h5 class="card-title">Recolección de víveres no perecederos</h5>
+                            <p class="card-text">Sábado 10 de agosto</p>
+                            <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
+                            <p class="card-text">Teléfono: +506 8888-8888</p>
+                            <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
+                        </div>
+                        <div class="map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15720.035903368916!2d-84.0350103!3d9.9332099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3f47ea4ff37%3A0x7a7818a6a9e5c90c!2sUniversidad%20Fid%C3%A9litas!5e0!3m2!1ses!2scr!4v1721068241124!5m2!1ses!2scr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        </div>
                     </div>
-                    <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15720.035903368916!2d-84.0350103!3d9.9332099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3f47ea4ff37%3A0x7a7818a6a9e5c90c!2sUniversidad%20Fid%C3%A9litas!5e0!3m2!1ses!2scr!4v1721068241124!5m2!1ses!2scr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR60W_-46cf_mNiRJ25M4p-xuUy8SfkUKMMSA&s" class="card-img-top" alt="Imagen actividad">
+                        <div class="card-body">
+                            <h5 class="card-title">Comedor público</h5>
+                            <p class="card-text">Sábado 31 de agosto</p>
+                            <p class="card-text">Universidad Fidelitas de Costa Rica, San Pedro</p>
+                            <p class="card-text">Teléfono: +506 8888-8888</p>
+                            <p class="card-text">Correo electrónico: inmigra@gmail.com</p>
+                        </div>
+                        <div class="map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15720.035903368916!2d-84.0350103!3d9.9332099!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0e3f47ea4ff37%3A0x7a7818a6a9e5c90c!2sUniversidad%20Fid%C3%A9litas!5e0!3m2!1ses!2scr!4v1721068241124!5m2!1ses!2scr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     <footer>
         <?php include 'plantillafooter.php'; ?>
     </footer>
@@ -70,4 +122,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
