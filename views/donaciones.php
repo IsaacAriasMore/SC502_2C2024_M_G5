0ac624c51e9750995a52b35a89bf8c08ef3a8e1e
JoseHuertas <huertasjose40@gmail.com>
2024-07-17 15:08:53 -0600

    correcion formularios

--- views/donaciones.php
+++ views/donaciones.php
@@ -10,7 +10,6 @@
 </head>
 
 <body>
-<<<<<<< HEAD
   <?php include 'plantilla.php'; ?>
   <aside class="iz" style="margin-top: 0px; background-color: #04282c; float: left;  width: 380px; height: 1600px; text-align: center; align-items: center;">
     <br>
@@ -104,114 +103,6 @@
     </div></div>
   <h3 style="margin-top:100px; margin-bottom: 50px; font-style: italic; text-align: center;">"Las pequeñas ayudas tienen grandes recompensas"</h3>
 
-=======
-  <?php
-  include 'plantilla.php';
-  ?>
-
-
-
-    <h1 style="text-align: center; margin-top: 10%; margin-bottom: -8%;">Donaciones</h1>
-    <div class="iniciov">
-      <div class="inicio">
-        <div class="forms1">
-          <form action="./ADMdonaciones.php" method="post">
-            <fieldset>
-
-              <legend><span class="number">1</span> Info Basica</legend>
-
-              <label for="name">Nombre:</label>
-              <input type="text" id="nombre" name="user_nombre">
-
-              <label for="name">Apellidos:</label>
-              <input type="text" id="nombre" name="user_nombre">
-
-              <label for="mail">Email:</label>
-              <input type="email" id="mail" name="user_email">
-
-              <label for="mail">Telefono:</label>
-              <input type="tel" id="tel" name="user_tel">
-
-            </fieldset>
-          </form>
-        </div>
-        <div class="forms2">
-          <form action="./ADMdonaciones.php" method="post">
-            <fieldset>
-
-              <legend><span class="number">2</span>Detalles</legend>
-
-              <div class="mb-3">
-                <label for="donacion" class="form-label">tipo de donación</label>
-                <select class="form-select" id="donacion" onchange="updateTotal()">
-                  <option value="0">Seleccione...</option>
-                  <option value="5000">Canasta de víveres</option>
-                  <option value="2000">Pasajes</option>
-                  <option value="10000">Hospedaje</option>
-                  <option value="otros">Otros</option>
-                </select>
-              </div>
-              <div class="mb-3" id="otherAmountContainer">
-              </div>
-              <div class="mb-3">
-                <label for="totalAmount" class="form-label">Total a donar</label>
-                <input type="text" class="form-control" id="totalAmount">
-              </div>
-
-              <label for="metodoPago" class="form-label">Método de Pago</label>
-              <select class="form-select" id="metodoPago" onchange="updatePaymentMethod()">
-                <option value="0">Seleccione...</option>
-                <option value="sinpe">SINPE Móvil</option>
-                <option value="tarjeta">Tarjeta</option>
-              </select>
-
-              <div class="mb-3" id="sinpeContainer" style="display: none;">
-                <label for="sinpeNumber" class="form-label">Número SINPE Móvil</label>
-                <input type="text" class="form-control" id="sinpeNumber" placeholder="Ingrese el número SINPE">
-              </div>
-
-              <div class="mb-3" id="tarjetaContainer" style="display: none;">
-                <label for="tarjetaNumber" class="form-label">Número de Tarjeta</label>
-                <input type="text" class="form-control" id="tarjetaNumber" placeholder="Ingrese el número de tarjeta">
-              </div>
-
-
-
-
-            </fieldset>
-
-
-
-
-
-          </form>
-
-
-
-        </div>
-
-        <div class="explicacion">
-          <H1 class="text-center" style="text-align:center; text-shadow: 1px 1px 1px grey; font-size: 18px; margin-top: 5%;">Como realizar la donacion</H1>
-          <div class="text2" style="text-align:center; width:30%; margin:auto;">
-            <legend class="text2"><span class="number1"></span> Ingresa tu información personal</legend>
-            <legend class="text2"><span class="number1"></span> Selecciona el tipo de donación</legend>
-            <legend class="text2"><span class="number1"></span> Explora las opciones de donación</legend>
-            <legend class="text2"><span class="number1"></span> Especifica la cantidad</legend>
-            <legend class="text2"><span class="number1"></span>Verifica la información ingresada</legend>
-            <legend class="text2"><span class="number1"></span> Envíar</legend>
-          </div>
-
-
-
-        </div>
-
-      </div>
-      <button type="button" id="btn-submit" class="btn btn-outline-success btn-lg">ENVIAR</button>
-
-    </div>
-
-    <h3 style="margin-top:100px; margin-bottom: 50px; font-style: italic; text-align: center;">"Las pequeñas ayudas tienen grandes recompensas"</h3>
->>>>>>> 9162674fe86fc1840118501cbe406632d6200cfd
 
   <footer>
     <?php
