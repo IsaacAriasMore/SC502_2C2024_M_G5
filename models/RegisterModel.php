81caadb5b24c2b6e24efed5cc162c850154042a7
Sebastián Merino Rodríguez <98624720+Semero34@users.noreply.github.com>
2024-08-18 10:02:46 -0600

    Registrarse Actualizado

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -9,7 +9,8 @@ class RegisterModel {
     public function registrar($nombre, $apellido, $correo, $telefono, $contrasena) {
         $hashedPassword = password_hash($contrasena, PASSWORD_BCRYPT);
         
-        $sql = "INSERT INTO users (nombre, apellido, correo, telefono, contrasena) VALUES (:nombre, :apellido, :correo, :telefono, :contrasena)";
+        $sql = "INSERT INTO usuarios (Nombre, Apellido, Correo, Telefono, Contrasena, FechaRegistro, estado, rol) 
+                VALUES (:nombre, :apellido, :correo, :telefono, :contrasena, NOW(), 1, 'usuario')";
         $stmt = $this->db->prepare($sql);
 
         return $stmt->execute([
